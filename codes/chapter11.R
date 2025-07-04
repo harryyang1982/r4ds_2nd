0c02696dc9018097e6f294be7d1e0f22704ed662
harryyang1982 <harry.seunghoon.yang@gmail.com>
2025-07-04 15:54:38 +0900

    chapter11 init

---
+++ codes/chapter11.R
@@ -0,0 +1,152 @@
+library(tidyverse)
+library(scales)
+library(ggrepel)
+library(patchwork)
+
+## Labels
+
+ggplot(mpg, aes(x = displ,
+                y = hwy)) +
+  geom_point(aes(color = class)) +
+  labs(
+    x = "Engine Displacement (L)",
+    y = "Highway fuel economy (mpg)",
+    color = "Car type",
+    title = "Fuel efficiency generally decrease with engine size",
+    subtitle = "Two seaters (sports cars) are an exception because of their light weight",
+    caption = "Data from fueleconomy.gov"
+  )
+
+df <- tibble(
+  x = 1:10,
+  y = cumsum(x^2)
+)
+
+ggplot(df, aes(x, y)) +
+  geom_point() +
+  labs(
+    x = quote(x[i]),
+    y = quote(sum(x[i] ^ 2, i == 1, n))
+  )
+
+## Annotations
+
+label_info <- mpg |> 
+  group_by(drv) |> 
+  arrange(desc(displ)) |> 
+  slice_head(n = 1) |> 
+  mutate(
+    drive_type = case_when(
+      drv == "f" ~ "front-wheel drive",
+      drv == "r" ~ "rear-wheel drive",
+      drv == "4" ~ "4-wheel drive"
+    )
+  ) |> 
+  select(displ, hwy, drv, drive_type)
+
+label_info
+
+ggplot(mpg, aes(x = displ,
+                y = hwy,
+                color = drv)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = FALSE) +
+  geom_text(
+    data = label_info,
+    aes(x = displ, y = hwy, label = drive_type),
+    fontface = "bold", size = 5, hjust = "right", vjust = "bottom"
+  ) +
+  theme(legend.position = "none")
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = FALSE) +
+  geom_label_repel(
+    data = label_info,
+    aes(x = displ, y = hwy, label = drive_type),
+    fontface = "bold", size = 5, nudge_y = 2
+  ) +
+  theme(legend.position = "none")
+
+potential_outliers <- mpg |>
+  filter(hwy > 40 | (hwy > 20 & displ > 5))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  geom_text_repel(data = potential_outliers, aes(label = model)) +
+  geom_point(data = potential_outliers, color = "red") +
+  geom_point(data = potential_outliers,
+             color = "red", size = 3, shape = "circle open")
+
+trend_text <- "Larger engine sizes tend to\nhave lower fuel economy." |>
+  str_wrap(width = 30)
+trend_text
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  annotate(
+    geom = "label", x = 3.5, y = 38,
+    label = trend_text,
+    hjust = "left", color = "red"
+  ) +
+  annotate(
+    geom = "segment",
+    x = 3, y = 35, xend = 5, yend = 25, color = "red",
+    arrow = arrow(type = "closed")
+  )
+
+## Scales
+### Default Scales
+
+ggplot(mpg, aes(x = displ,
+                y = hwy)) +
+  geom_point(aes(color = class))
+
+#### as sames as below
+
+ggplot(mpg, aes(x = displ,
+                y = hwy)) +
+  geom_point(aes(color = class)) +
+  scale_x_continuous() +
+  scale_y_continuous() +
+  scale_color_discrete()
+
+### Axis Ticks and Legend Keys
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  scale_y_continuous(breaks = seq(15, 40, by = 5))
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  scale_x_continuous(labels = NULL) +
+  scale_y_continuous(labels = NULL) +
+  scale_color_discrete(labels = c("4" = "4-wheel",
+                                  "f" = "front",
+                                  "r" = "rear"))
+
+left1 <- ggplot(diamonds, aes(x = price, y = cut)) +
+  geom_boxplot(alpha = 0.05) +
+  scale_x_continuous(labels = label_dollar())
+
+right1 <- ggplot(diamonds, aes(x = price, y = cut)) +
+  geom_boxplot(alpha = 0.05) +
+  scale_x_continuous(
+    labels = label_dollar(scale = 1/1000, suffix = "K"),
+    breaks = seq(1000, 19000, by = 6000)
+  )
+
+library(gridExtra)
+grid.arrange(left1, right1, ncol = 2)
+
+ggplot(diamonds, aes(x = cut, fill = clarity)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(name = "Percentage",
+                     labels = label_percent())
+
+presidential |>
+  mutate(id = 33 + row_number()) |>
+  ggplot(aes(x = start, y = id)) +
+  geom_point() +
+  geom_segment(aes(xend = end, yend = id)) +
+  scale_x_date(name = NULL, breaks = presidential$start, date_labels = "'%y")
+
