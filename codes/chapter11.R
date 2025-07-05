6919e786747f2f432918fe0514b018fa408562a4
harryyang1982 <harry.seunghoon.yang@gmail.com>
2025-07-05 14:19:11 +0900

    chapter 11 cleared

--- codes/chapter11.R
+++ codes/chapter11.R
@@ -150,3 +150,233 @@ presidential |>
   geom_segment(aes(xend = end, yend = id)) +
   scale_x_date(name = NULL, breaks = presidential$start, date_labels = "'%y")
 
+### Legend Layout
+
+base <- ggplot(mpg, aes(x = displ,
+                        y = hwy)) +
+  geom_point(aes(color = class))
+
+base + theme(legend.position = "right")
+base + theme(legend.position = "left")
+base + 
+  theme(legend.position = "top") +
+  guides(color = guide_legend(nrow = 3))
+base + 
+  theme(legend.position = "bottom") +
+  guides(color = guide_legend(nrow = 3))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme(legend.position = "bottom") +
+  guides(color = guide_legend(nrow = 2, override.aes = list(size = 4)))
+
+### Replacing a Scale
+
+left2 <- ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_bin2d()
+
+right2 <- ggplot(diamonds, aes(x = log10(carat), y = log10(price))) +
+  geom_bin2d()
+
+grid.arrange(left2, right2, ncol = 2)
+
+ggplot(diamonds, aes(x = carat, y = price)) +
+  geom_bin2d() +
+  scale_x_log10() +
+  scale_y_log10()
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv))
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  scale_color_brewer(palette = "Set1")
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv, shape = drv)) +
+  scale_color_brewer(palette = "Set1")
+
+presidential |> 
+  mutate(id = 33 + row_number()) |> 
+  ggplot(aes(x = start,
+             y = id,
+             color = party)) +
+  geom_point() +
+  geom_segment(aes(xend = end,
+                   yend = id)) +
+  scale_color_manual(values = c(Republican = "#E81B23",
+                                Democratic = "#00AEF3"))
+
+### Viridis
+df <- tibble(
+  x = rnorm(10000),
+  y = rnorm(10000)
+)
+
+left3 <- ggplot(df, aes(x, y)) +
+  geom_hex() +
+  coord_fixed() +
+  labs(title = "Default, continuous", x = NULL, y = NULL)
+
+middle3 <- ggplot(df, aes(x, y)) +
+  geom_hex() +
+  coord_fixed() +
+  scale_fill_viridis_c() +
+  labs(title = "Viridis, continuous", x = NULL, y = NULL)
+
+right3 <- ggplot(df, aes(x, y)) +
+  geom_hex() +
+  coord_fixed() +
+  scale_fill_viridis_b() +
+  labs(title = "Viridis, binned", x = NULL, y = NULL)
+
+grid.arrange(left3, middle3, right3,
+             nrow = 1)
+
+left4 <- ggplot(mpg, aes(x = displ,
+                         y = hwy)) +
+  geom_point(aes(color = drv)) +
+  geom_smooth()
+
+right4 <- mpg |> 
+  filter(displ >= 5 & displ <= 6 & hwy >= 10 & hwy <= 25) |> 
+  ggplot(aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  geom_smooth()
+
+grid.arrange(left4, right4, nrow = 1)
+
+# Left
+left5 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  geom_smooth() +
+  scale_x_continuous(limits = c(5, 6)) +
+  scale_y_continuous(limits = c(10, 25))
+# Right
+right5 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = drv)) +
+  geom_smooth() +
+  coord_cartesian(xlim = c(5, 6), ylim = c(10, 25))
+
+grid.arrange(left5, right5,
+             nrow = 1)
+
+suv <- mpg |> filter(class == "suv")
+compact <- mpg |> filter(class == "compact")
+
+# Left
+
+left6 <- ggplot(suv, aes(x = displ, y = hwy, color = drv)) +
+  geom_point()
+
+# Right
+
+right6 <- ggplot(compact, aes(x = displ, y = hwy, color = drv)) +
+  geom_point()
+
+grid.arrange(left6, right6, nrow = 1)
+
+x_scale <- scale_x_continuous(limits = range(mpg$displ))
+y_scale <- scale_y_continuous(limits = range(mpg$hwy))
+col_scale <- scale_color_discrete(limits = unique(mpg$drv))
+
+# Left
+left7 <- ggplot(suv, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  x_scale +
+  y_scale +
+  col_scale
+
+# Right
+right7 <- ggplot(compact, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  x_scale +
+  y_scale +
+  col_scale
+
+grid.arrange(left7, right7, nrow = 1)
+
+ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point(aes(color = class)) +
+  geom_smooth(se = FALSE) +
+  theme_bw()
+
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point() +
+  labs(
+    title = "Larger engine sizes tend to have lower fuel economy",
+    caption = "Source: https://fueleconomy.gov."
+  ) +
+  theme(
+    legend.position = c(0.6, 0.7),
+    legend.direction = "horizontal",
+    legend.box.background = element_rect(color = "black"),
+    plot.title = element_text(face = "bold"),
+    plot.title.position = "plot",
+    plot.caption.position = "plot",
+    plot.caption = element_text(hjust = 0)
+  )
+
+## Layout
+
+p1 <- ggplot(mpg, aes(x = displ, 
+                      y = hwy)) +
+  geom_point() +
+  labs(title = "Plot 1")
+
+p2 <- ggplot(mpg, aes(x = drv, 
+                      y = hwy)) +
+  geom_boxplot() +
+  labs(title = "Plot 2")
+
+p1 + p2
+
+p3 <- ggplot(mpg, aes(x = cty, y = hwy)) +
+  geom_point() +
+  labs(title = "Plot 3")
+(p1 | p3) / p2
+
+p1 <- ggplot(mpg, aes(x = drv, y = cty, color = drv)) +
+  geom_boxplot(show.legend = FALSE) +
+  labs(title = "Plot 1")
+
+p2 <- ggplot(mpg, aes(x = drv, y = hwy, color = drv)) +
+  geom_boxplot(show.legend = FALSE) +
+  labs(title = "Plot 2")
+
+p3 <- ggplot(mpg, aes(x = cty, color = drv, fill = drv)) +
+  geom_density(alpha = 0.5) +
+  labs(title = "Plot 3")
+
+p4 <- ggplot(mpg, aes(x = hwy, color = drv, fill = drv)) +
+  geom_density(alpha = 0.5) +
+  labs(title = "Plot 4")
+
+p5 <- ggplot(mpg, aes(x = cty, y = hwy, color = drv)) +
+  geom_point(show.legend = FALSE) +
+  facet_wrap(~drv) +
+  labs(title = "Plot 5")
+
+(guide_area() / (p1 + p2) / (p3 + p4) / p5) +
+  plot_annotation(
+    title = "City and highway mileage for cars with different drivetrains",
+    caption = "Source: https://fueleconomy.gov."
+  ) +
+  plot_layout(
+    guides = "collect",
+    heights = c(1, 3, 2, 4)
+  ) &
+  theme(legend.position = "top")
+
+p1 <- ggplot(mpg, aes(x = displ, y = hwy)) +
+  geom_point() +
+  labs(title = "Plot 1")
+p2 <- ggplot(mpg, aes(x = drv, y = hwy)) +
+  geom_boxplot() +
+  labs(title = "Plot 2")
+p3 <- ggplot(mpg, aes(x = cty, y = hwy)) +
+  geom_point() +
+  labs(title = "Plot 3")
+(p1 | p2) / p3
+
